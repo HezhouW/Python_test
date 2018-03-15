99440132bba877838b238ba03aeb1b5ff1ade010
whz_mac <hnqxwhz@163.com>
2018-03-15 15:49:12 +0800

    mac上的改动提交

--- b.py
+++ b.py
@@ -1,6 +1,3 @@
-import a
-#a.path.append("H:\\python\project")
-#print a.path
 num = 20
 numc = a.numplus(num)
 print num,numc
--- child/a.py
+++ child/a.py
@@ -1,5 +1,4 @@
-# -*- coding: GBK -*-
-"这是一个被引用用的测试文件"
+# -*- coding: utf-8 -*-
 def numplus(num):
     return num+10
 print "a.py run..."
